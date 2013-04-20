f12045f48f840285d0395eebbff096788971a8ae
Partus <partus@mail.com>
2013-04-20 19:57:06 +0400

    ..

--- package.js
+++ package.js
@@ -3,8 +3,9 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.use('jquery', 'client');
+  api.use('jquery', 'client','underscore');
   api.add_files([
+    'jsonform/deps/opt/jsv.js',
     'jsonform/lib/jsonform.js'
   ], ['client']
   );
