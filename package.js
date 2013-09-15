92df5738637c6809c7218fa619c04e08bd5077e2
partus <gmenua@gmail.com>
2013-09-16 03:45:10 +0400

    Update package.js

--- package.js
+++ package.js
@@ -3,7 +3,8 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.use('jquery', 'client','underscore');
+  api.use('underscore', 'client');
+  api.use('jquery', 'client');
   api.add_files([
     'jsonform/deps/opt/jsv.js',
     'jsonform/lib/jsonform.js'
